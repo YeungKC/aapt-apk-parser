942402ce63e7639e5c550ffc804db63dca66544f
YeungKC <I@YeungKC.com>
2018-06-13 21:00:23 +0800

    fix: 修复 bug 和 更新版本

--- bin/aapt.js
+++ bin/aapt.js
@@ -4,9 +4,9 @@ const path = require('path');
 const os   = require('os');
 const exec = require('child_process').exec;
 
-module.exports = function(apkPath, aapt) {
+module.exports = function(apkPath, aaptPath) {
   return new Promise((resolve, reject) => {
-    const aapt    = aapt || path.join(__dirname, 'aapt_' + os.platform());
+    const aapt    = aaptPath || path.join(__dirname, 'aapt_' + os.platform());
     const command = aapt + ' d badging ' + apkPath;
 
     exec(command, function(err, stdout, stderr) {
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const aapt          = require('./bin/aapt');
 const parseText     = require('./bin/parseText');
 const writeIconFile = require('./bin/writeIconFile');
 
-async function parse(apkPath, aapt) {
-  let stdout = await aapt(apkPath, aapt);
+async function parse(apkPath, aaptPath) {
+  let stdout = await aapt(apkPath, aaptPath);
   stdout     = stdout.replace(/'/g, '');
   return parseText(stdout, '\n', ':');
 }
