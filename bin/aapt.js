58c11ee7cb9f6717ed7fa5a69aa4bd02492d2ca9
YeungKC <I@YeungKC.com>
2018-06-13 22:45:28 +0800

    refactor: 增加 chmod 流程和更新版本

--- bin/aapt.js
+++ bin/aapt.js
@@ -3,10 +3,17 @@
 const path = require('path');
 const os   = require('os');
 const exec = require('child_process').exec;
+const fs   = require('fs');
 
 module.exports = function(apkPath, aaptPath) {
   return new Promise((resolve, reject) => {
-    const aapt    = aaptPath || path.join(__dirname, 'aapt_' + os.platform());
+    const aapt = aaptPath || path.join(__dirname, 'aapt_' + os.platform());
+
+    try {
+      fs.chmodSync(aapt, '777');
+    } catch (e) {
+    }
+
     const command = aapt + ' d badging ' + apkPath;
 
     exec(command, function(err, stdout, stderr) {
