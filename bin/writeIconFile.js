9d20836b743c3563d75abc872de5abb18fb3c54d
YeungKC <I@YeungKC.com>
2018-06-13 17:53:41 +0800

    feat: writeIconFile 返回值增加 mime 字段

--- bin/writeIconFile.js
+++ bin/writeIconFile.js
@@ -2,14 +2,16 @@
 
 const decompress = require('decompress');
 const path       = require('path');
+const mime       = require('mime-types');
 
 module.exports = async function(apkFilePath, iconResPath, iconFilePath) {
-  return await decompress(apkFilePath, path.dirname(iconFilePath), {
+  const file = await decompress(apkFilePath, path.dirname(iconFilePath), {
     filter: file => file.path === iconResPath,
     map   : file => {
-      file.path = `${path.basename(iconFilePath, path.extname(iconFilePath))}${path.extname(file.path)}`;
+      file.path = iconFilePath = `${path.basename(iconFilePath, path.extname(iconFilePath))}${path.extname(file.path)}`;
       return file;
     },
   });
+  return {...file, mime: mime.lookup(iconFilePath)};
 };
 
