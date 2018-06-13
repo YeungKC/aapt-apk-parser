5aec1d4dbd69ed7c2de8d13d6052b5cc2a35a4d7
YeungKC <I@YeungKC.com>
2018-06-13 18:40:12 +0800

    fix: 修复 path 路径错误的问题

--- bin/writeIconFile.js
+++ bin/writeIconFile.js
@@ -5,13 +5,15 @@ const path       = require('path');
 const mime       = require('mime-types');
 
 module.exports = async function(apkFilePath, iconResPath, iconFilePath) {
-  const file = await decompress(apkFilePath, path.dirname(iconFilePath), {
+  const file        = await decompress(apkFilePath, path.dirname(iconFilePath), {
     filter: file => file.path === iconResPath,
     map   : file => {
-      file.path = iconFilePath = `${path.basename(iconFilePath, path.extname(iconFilePath))}${path.extname(file.path)}`;
+      file.path = `${path.basename(iconFilePath, path.extname(iconFilePath))}${path.extname(file.path)}`;
       return file;
     },
   });
-  return {...file[0], mime: mime.lookup(iconFilePath)};
+  const fileElement = file[0];
+  fileElement.path = path.join(path.dirname(iconFilePath), fileElement.path);
+  return {...fileElement, mime: mime.lookup(fileElement.path)};
 };
 
